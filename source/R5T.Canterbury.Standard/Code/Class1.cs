ea7691e17ed217e380e3993de381d471339d667a
MinexAutomation <minex.automation@gmail.com>
2020-01-22 16:16:00 -0800

    Added IServiceCollection extensions.

--- source/R5T.Canterbury.Standard/Code/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace R5T.Canterbury.Standard
-{
-    public class Class1
-    {
-    }
-}
---
+++ source/R5T.Canterbury.Standard/Code/Services/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Canterbury.Default;
+
+
+namespace R5T.Canterbury.Standard
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static IServiceCollection AddCDriveNameConvention(this IServiceCollection services)
+        {
+            services.AddSingleton<ICDriveNameConvention, CDriveNameConvention>();
+
+            return services;
+        }
+    }
+}
