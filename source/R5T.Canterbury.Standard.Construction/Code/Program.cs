af6f415441328d56f9211a47ae82bb22b5443a65
MinexAutomation <minex.automation@gmail.com>
2020-01-22 16:12:19 -0800

    Initial commit of source-stub.

---
+++ source/R5T.Canterbury.Standard.Construction/Code/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace R5T.Canterbury.Standard.Construction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
