64440c46297f791603b9d2c0ec3743f42f33234d
MinexAutomation <minex.automation@gmail.com>
2020-01-30 01:20:21 -0800

    Improved IServiceCollection extensions to support ServiceAction<T> funcctionality.

--- source/R5T.Canterbury.Standard/Code/Services/Extensions/IServiceCollectionExtensions.cs
+++ source/R5T.Canterbury.Standard/Code/Services/Extensions/IServiceCollectionExtensions.cs
@@ -3,17 +3,30 @@
 using Microsoft.Extensions.DependencyInjection;
 
 using R5T.Canterbury.Default;
+using R5T.Dacia;
 
 
 namespace R5T.Canterbury.Standard
 {
     public static class IServiceCollectionExtensions
     {
+        /// <summary>
+        /// Adds the <see cref="CDriveNameConvention"/> implementation of <see cref="ICDriveNameConvention"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
         public static IServiceCollection AddCDriveNameConvention(this IServiceCollection services)
         {
-            services.AddSingleton<ICDriveNameConvention, CDriveNameConvention>();
+            services.AddDefaultCDriveNameConvention();
 
             return services;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="CDriveNameConvention"/> implementation of <see cref="ICDriveNameConvention"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static ServiceAction<ICDriveNameConvention> AddCDriveNameConventionAction(this IServiceCollection services)
+        {
+            var serviceAction = new ServiceAction<ICDriveNameConvention>(() => services.AddCDriveNameConvention());
+            return serviceAction;
         }
     }
 }
